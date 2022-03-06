c6641119d6c9182439758c828ee099392bfe7388
BorjaG90 <borjag90dev@gmail.com>
2022-03-06 14:20:18 +0100

    feat(CounterApp.js): task add substract & reset buttons

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -3,19 +3,30 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
 
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(value);
 
   // handleAdd
   const handleAdd = () => {
     // setCounter(counter + 1);
-    setCounter( (c)=> c +1 );
+    setCounter( (c)=> c + 1 );
   }
+
+  const handleReset = () => {
+    setCounter( c => value);
+  }
+
+  const handleSubstract = () => {
+    setCounter( c => c - 1 )
+  }
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{ counter }</h2>
 
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleSubstract}>-1</button>
     </>
   );
 }
