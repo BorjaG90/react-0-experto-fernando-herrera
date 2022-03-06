fd7e170cf1164f338ceef78cb30a334aaae9fdfd
BorjaG90 <borjag90dev@gmail.com>
2022-03-06 13:49:57 +0100

    feat(*): propTypes

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,13 +1,22 @@
 // Functional Components
 import React from "react";
+import PropTypes from 'prop-types';
 
-const PrimeraApp = ({saludo = "Hola mundo"}) => {
+const PrimeraApp = ({saludo}) => {
+  if (!saludo) {
+    throw new Error('El saludo es necesario');
+  }
   return (
     <>
       <h1>{saludo}</h1>
       <p>Mi primera aplicación</p>
     </>
   );
+}
+
+// Definicion de las props del componente
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired
 }
 
 export default PrimeraApp;
