8ac01c1a5f4bda34161c61f4b52d3badda5204af
BorjaG90 <borjag90dev@gmail.com>
2022-03-06 13:46:11 +0100

    feat(PrimeraApp.js): props comm

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,13 +1,10 @@
 // Functional Components
 import React from "react";
 
-const PrimeraApp = () => {
-  const saludo = 'Hola Mundo';
-  const perfil = { nombre: 'Borja', edad: 31 };
+const PrimeraApp = ({saludo = "Hola mundo"}) => {
   return (
     <>
       <h1>{saludo}</h1>
-      <pre>{ JSON.stringify(perfil, null, 3)}</pre>
       <p>Mi primera aplicación</p>
     </>
   );
