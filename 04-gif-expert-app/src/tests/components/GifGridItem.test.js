ed0ac28c5ecde1c4a2d0855ed2e2f5005ea0418d
BorjaG90 <borjag90dev@gmail.com>
2022-04-07 08:50:27 +0200

    test(GifGridItem.test.js): add properties

--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -2,8 +2,11 @@ import {shallow} from 'enzyme'
 import {GifGridItem} from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => { 
+  const title = 'Un título';
+  const url = 'http://localhost/algo.jpg';
+
   test('should de mostrar el componente correctamente', () => { 
-    const wrapper = shallow(<GifGridItem />);
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect ( wrapper ).toMatchSnapshot();
    })
  })
