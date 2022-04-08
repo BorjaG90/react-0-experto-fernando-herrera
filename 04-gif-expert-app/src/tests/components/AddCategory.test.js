3d0a7a2026eed92d30f0fcfea5d12c321fe6f7e0
BorjaG90 <borjag90dev@gmail.com>
2022-04-08 08:58:53 +0200

    test(AddCategory.test.js): add input test

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -3,10 +3,18 @@ import {shallow} from 'enzyme';
 
 import {AddCategory} from '../../components/AddCategory';
 
-describe('Pruebas en <AddCategory />', ()=>{
-  const setCategories = () => {}
+describe('Pruebas en <AddCategory />', () => {
+  const setCategories = () => {};
+  const wrapper = shallow( <AddCategory setCategories={setCategories} />);
+
   test('debe de mostrarse correctamente', () => { 
-    const wrapper = shallow( <AddCategory setCategories={setCategories} />);
     expect(wrapper).toMatchSnapshot();
-  })
+  });
+
+  test('debe de cambiar la caja de texto', () => { 
+    const input = wrapper.find('input');
+    const value = 'Hola Mundo';
+
+    input.simulate('change', { target: { value } });
+  });
 })
