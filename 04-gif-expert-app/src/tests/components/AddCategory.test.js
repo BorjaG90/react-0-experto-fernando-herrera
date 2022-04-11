4c0955fd56e91d9596ae00bcc2c8a6ab82dd7b4d
BorjaG90 <borjag90dev@gmail.com>
2022-04-11 09:01:40 +0200

    test(AddCategory.test.js): simulate submit & clean input

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -29,4 +29,17 @@ describe('Pruebas en <AddCategory />', () => {
 
     expect(setCategories).not.toHaveBeenCalled();
   });
+
+  test('should llamar el setCategories y limpiar la caja de texto', () => { 
+    const input = wrapper.find('input');
+    const value = 'Valor de prueba';
+    // Simular el inputChange
+    input.simulate('change', { target: { value } });
+    // Simular el submit
+    wrapper.find('form').simulate('submit', { preventDefault(){} });
+    // SetCategories se debe haber llamado con una función
+    expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+    // El valor del input debe estar vacio ''
+    expect(input.prop('value')).toBe('');
+   })
 })
