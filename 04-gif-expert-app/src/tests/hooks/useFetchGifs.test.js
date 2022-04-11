beafbd95d163b09bb4fcd3e19e4a11269b736460
BorjaG90 <borjag90dev@gmail.com>
2022-04-11 11:03:40 +0200

    test(useFetchGifs.test.js): add waitForNextUpdate in customHook

--- 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -4,13 +4,25 @@ import {shallow} from 'enzyme';
 
 import {useFetchGifs} from '../../hooks/useFetchGifs';
 
-describe('Preubas en el hook useFetchGifs', () => { 
-  test('should de retornar el estado inicial', () => { 
+describe('Pruebas en el hook useFetchGifs', () => { 
+  test('should de retornar el estado inicial', async () => { 
     // const { data, loading } = useFetchGifs( 'Dragon Ball' );
-    const {result} = renderHook( () => useFetchGifs( 'Dragon Ball' ));
+    const {result, waitForNextUpdate} = renderHook( () => useFetchGifs( 'Dragon Ball' ));
     const {data, loading} = result.current;
+    await waitForNextUpdate();
 
     expect( data ).toEqual([]);
     expect(loading).toBeTruthy;
-   })
+  });
+
+  test('debe de retornar un arreglo de imgs y el loading en false', async () => { 
+    const {result, waitForNextUpdate} = renderHook( () => 
+      useFetchGifs( 'Dragon Ball' )
+    );
+    await waitForNextUpdate();
+    const {data, loading} = result.current;
+
+    expect( data.length ).toBe(10);
+    expect(loading).toBeFalsy;
+  })
  })
