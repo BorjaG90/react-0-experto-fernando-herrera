92443d97900738616e9a4a2f301f526c8b606d1f
BorjaG90 <borjag90dev@gmail.com>
2022-04-11 10:29:13 +0200

    feat(GifExpertApp.js): update app

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
-const GifExpertApp = () => {
+export const GifExpertApp = ({defaultCategories = []}) => {
   //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
-  const [categories, setCategories] = useState(['Dragon Ball']);
+  const [categories, setCategories] = useState(defaultCategories);
 
   // const handleAdd = () => {
   //   // setCategories( [...categories, 'Pokemon'] );
@@ -32,5 +32,3 @@ const GifExpertApp = () => {
     </>
   );
 };
-
-export default GifExpertApp;
