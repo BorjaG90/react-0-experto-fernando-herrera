c7e2df04ab4d3d602d0ad1a57a2f76afe588ed0c
BorjaG90 <borjag90dev@gmail.com>
2022-04-14 15:14:04 +0200

    feat(*): useState

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { HookApp } from './HookApp';
+// import { HookApp } from './HookApp';
+import { CounterApp } from './components/01-useState/CounterApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HookApp />
+    <CounterApp />
 );
