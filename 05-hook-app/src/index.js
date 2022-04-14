d5ec995b6ffa2d5af0116a448ffea15a17bbe3d0
BorjaG90 <borjag90dev@gmail.com>
2022-04-14 15:27:59 +0200

    feat(*): useCounter - custom hook

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import { HookApp } from './HookApp';
-import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <CounterApp />
+    <CounterWithCustomHook />
 );
