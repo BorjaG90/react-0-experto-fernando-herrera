5248669632d42d2257dfde59966db3aab222db73
BorjaG90 <borjag90dev@gmail.com>
2022-04-14 12:27:17 +0200

    refactor(index.js): update to React18 functionality

--- 04-gif-expert-app/src/index.js
+++ 04-gif-expert-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import GifExpertApp from './GifExpertApp';
 
 import './index.css';
 
-ReactDOM.render( <GifExpertApp />, document.getElementById('root') );
+createRoot(document.querySelector('#root')).render( <GifExpertApp />);
