20f0d863c63ca133d4be2ffaf6dbd8335896f97f
BorjaG90 <borjag90dev@gmail.com>
2022-04-19 15:31:58 +0200

    feat(*): useLayoutEffect

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -8,7 +8,8 @@ import ReactDOM from 'react-dom/client';
 // import { FormWithCustomHooks } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { LayoutEffect } from './components/05-useLayoutEffect/LayoutEffect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <RealExampleRef /> );
+root.render( <LayoutEffect /> );
