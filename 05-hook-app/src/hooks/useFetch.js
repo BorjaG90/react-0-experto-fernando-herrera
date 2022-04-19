df72ac56d35dd90d328a738df44ace4e3f428900
BorjaG90 <borjag90dev@gmail.com>
2022-04-19 15:19:40 +0200

    feat(*): useRef - real case use

--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -1,20 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const useFetch = (url) => {
-  const [state, setState] = useState({data: null, loading: true, error: null});
+  const isMounted = useRef(true);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
   useEffect(() => {
-    setState({data: null, loading: true, error: null});
-    
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+
     fetch(url)
-    .then(resp => resp.json())
-    .then(data => {
-      setState({
-        loading: false,
-        error: null,
-        data
-      })
-    })
-  }, [url])
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (isMounted.current) {
+          setState({
+            loading: false,
+            error: null,
+            data,
+          });
+        }
+      });
+  }, [url]);
 
   return state;
-}
+};
