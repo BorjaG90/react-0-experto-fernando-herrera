9012461eec34bbea3160ba41b001d057ba1401b2
BorjaG90 <borjag90dev@gmail.com>
2022-04-20 14:54:42 +0200

    feat(*): memo

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -9,7 +9,8 @@ import ReactDOM from 'react-dom/client';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-import { LayoutEffect } from './components/05-useLayoutEffect/LayoutEffect';
+// import { LayoutEffect } from './components/05-useLayoutEffect/LayoutEffect';
+import { Memorize } from './components/06-memos/Memorize';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <LayoutEffect /> );
+root.render( <Memorize /> );
