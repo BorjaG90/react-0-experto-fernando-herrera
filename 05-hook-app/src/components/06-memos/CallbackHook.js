ec14915dea96bd2abca3beb818b22ca2e8836e73
BorjaG90 <borjag90dev@gmail.com>
2022-04-21 10:08:01 +0200

    fix(CallbackHook.js): minor fix

--- 05-hook-app/src/components/06-memos/CallbackHook.js
+++ 05-hook-app/src/components/06-memos/CallbackHook.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { ShowIncrement } from './ShowIncrement';
 
 import '../02-useEffect/effects.css';
