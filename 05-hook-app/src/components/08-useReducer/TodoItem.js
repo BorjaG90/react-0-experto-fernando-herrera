7720ebb8e8eedcb219cc47b3158f8c216aed973b
BorjaG90 <borjag90dev@gmail.com>
2022-04-23 14:15:16 +0200

    feat(TodoItemjs): add TodoItem component

---
+++ 05-hook-app/src/components/08-useReducer/TodoItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const TodoItem = ({todo, index, handleDelete, handleToggle}) => {
+  return (
+    <li
+      key={todo.id}
+      className="list-group-item"
+    >
+      <p
+        className={`${todo.done && 'complete'}`}
+        onClick={()=> handleToggle(todo.id)}
+      >{index +1}. {todo.desc}</p>
+      <button
+        className="btn btn-danger"
+        onClick={() => (handleDelete(todo.id))}
+      >Borrar</button>
+    </li>
+  )
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  handleToggle: PropTypes.func.isRequired
+}
