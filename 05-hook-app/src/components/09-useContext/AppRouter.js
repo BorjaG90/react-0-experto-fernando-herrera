f408b12c08a49114ee919838905201b279daff81
BorjaG90 <borjag90dev@gmail.com>
2022-04-25 19:56:25 +0200

    feat(*): Link & NavLink

--- 05-hook-app/src/components/09-useContext/AppRouter.js
+++ 05-hook-app/src/components/09-useContext/AppRouter.js
@@ -3,36 +3,23 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
+  Navigate
 } from "react-router-dom";
 import { AboutScreen } from './AboutScreen';
 import { HomeScreen } from './HomeScreen';
 import { LoginScreen } from './LoginScreen';
+import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
   return (
   <Router>
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-      </nav>
-
-      {/* A <Routes> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
+      <NavBar />
       <Routes>
         <Route exact path="/about" element={<AboutScreen/>} />
-        <Route exact path="/users" element={<LoginScreen/>} />
+        <Route exact path="/login" element={<LoginScreen/>} />
         <Route exact path="/" element={<HomeScreen/>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   </Router>
