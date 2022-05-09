eeae68a0f1e4f1e921ee9e26b93609c7edc416a0
BorjaG90 <borjag90dev@gmail.com>
2022-05-09 14:22:17 +0200

    feat(*): add public routes #6

--- 07-heroes-app_17/src/routers/AppRouter.js
+++ 07-heroes-app_17/src/routers/AppRouter.js
@@ -3,19 +3,23 @@ import { Routes, Route ,BrowserRouter} from 'react-router-dom';
 
 import { DashboardRoutes } from './DashboardRoutes';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 import { LoginScreen } from '../components/login/LoginScreen';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/login" element={
+          <PublicRoute>
+            <LoginScreen />
+          </PublicRoute>
+        } />
         <Route path="/*" element={
           <PrivateRoute>
             <DashboardRoutes />
           </PrivateRoute>
         } />
-        {/* <Route path="/*" element={<DashboardRoutes />} /> */}
       </Routes>
     </BrowserRouter>
   )
