4f6002b802cfb24d0299959e9a32895d529b9d6c
BorjaG90 <borjag90dev@gmail.com>
2022-09-15 13:22:20 +0200

    feat(FirstApp.jsx): show variables in HTML #11

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,24 @@
+const newMessage = {
+  message: 'Hola Mundo',
+  title: 'Fernando'
+};
+
+/* Si no hay una dependencia con el 
+comportamiento interno del componente,
+es mejor sacar las funciones fuera del componente
+(en este caso FirstApp)
+*/
+const getResult = (a,b) => {
+  return a + b
+}
+
 export const FirstApp = () => {
   return (
     <>
-      <h2>Borja</h2>
+      <h2>{newMessage.title}</h2>
+      {/* <code>{ JSON.stringify(newMessage) }</code> */}
       <h3>Soy un subtítulo</h3>
+      <h3>{ getResult(3,5) }</h3>
     </>
   )
 }
