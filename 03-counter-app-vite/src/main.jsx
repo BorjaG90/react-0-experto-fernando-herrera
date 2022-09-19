1867dd02cf74947377126b6f8e1daa1553e201d2
BorjaG90 <borjag90dev@gmail.com>
2022-09-19 16:09:21 +0200

    feat(CounterApp, main): task - CounterApp component  #11

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+// import { FirstApp } from './FirstApp';
 // import { HelloWorldApp } from './HelloWorldApp';
 
 import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
   <React.StrictMode>
-    {/* <HelloWorldApp /> */}
-    <FirstApp />
+    {/* <HelloWorldApp /> 
+     <FirstApp /> */}
+    <CounterApp value={155}/>
   </React.StrictMode>
 );
