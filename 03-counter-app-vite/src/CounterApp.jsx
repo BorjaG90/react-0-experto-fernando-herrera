a6eb1d9efd3ea97e8b42f147edf7d23eff25e828
BorjaG90 <borjag90dev@gmail.com>
2022-09-19 16:50:39 +0200

    feat(CounterApp): event click #11

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
 
+const handleAdd = (event) => {
+  console.log(event);
+}
+
 export const CounterApp = ({value}) => {
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{ value }</h2>
+      <h2> { value } </h2>
+
+      <button onClick={ handleAdd }>
+        +1
+      </button>
     </>
   )
 }
