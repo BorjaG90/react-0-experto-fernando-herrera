4d8db8337b078c377da6420357add1107ccb525c
BorjaG90 <borjag90dev@gmail.com>
2022-09-19 09:42:37 +0200

    feat(FirstApp, main): default props #11

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -14,18 +14,26 @@ const getResult = (a,b) => {
   return a + b
 }
 
-export const FirstApp = ({title, subtitle}) => {
+export const FirstApp = ({title, subTitle, name}) => {
   return (
     <>
       <h2>{title}</h2>
       {/* <code>{ JSON.stringify(newMessage) }</code> */}
       {/* <h3>Soy un subtítulo</h3> */}
       {/* <h3>{ getResult(3,5) }</h3> */}
-      <h3>{subtitle}</h3>
+      <h3>{subTitle}</h3>
+      <p>{name}</p>
     </>
   )
 }
 
 FirstApp.propTypes = {
-  title: PropTypes.string.isRequired
+  subTitle: PropTypes.string,
+  title: PropTypes.string.isRequired,
+}
+
+FirstApp.defaultProps = {
+  name: 'Borja Gete',
+  subTitle: 'No hay subtítulo',
+  title: 'No hay título',
 }
