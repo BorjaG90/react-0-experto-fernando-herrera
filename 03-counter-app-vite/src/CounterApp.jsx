ae7f7ce2e56085389e69035e88c41488e8f8e754
BorjaG90 <borjag90dev@gmail.com>
2022-09-19 16:59:23 +0200

    feat(CounterApp): useState hook #11

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,19 +1,21 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const handleAdd = (event) => {
-  console.log(event);
-}
-
 export const CounterApp = ({value}) => {
+  const [counter, setCounter] = useState( value );
+
+  const handleAdd = (event) => {
+    // console.log(event)
+    // setCounter(counter + 1);
+    setCounter( (c) => c +1 );
+  }
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> { value } </h2>
+      <h2> { counter } </h2>
 
-      <button onClick={ handleAdd }>
-        +1
-      </button>
+      <button onClick={ handleAdd }>+1</button>
     </>
   )
 }
