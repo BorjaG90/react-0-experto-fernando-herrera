f683b186ce664235dd4b7c8bc7d7f3e38d560cbe
BorjaG90 <borjag90dev@gmail.com>
2022-09-26 20:43:28 +0200

    test(CounterApp.*): add simulate events test #12

--- 03-counter-app-vite/tests/CounterApp.test.jsx
+++ 03-counter-app-vite/tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 describe('Pruebas en <CounterApp />', () => {
@@ -12,5 +12,31 @@ describe('Pruebas en <CounterApp />', () => {
   test('debe de mostrar el valor inicial de 100', () => {
     render(<CounterApp value={100} />);
     expect(screen.getByText(100)).toBeTruthy();
-  })
+  });
+
+  test('debe de incrementar con el botón +1', () => {
+    render(<CounterApp value={initialValue} />);
+    // fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByRole('button', {name: "btn-add" }));
+    expect(screen.getByText('156')).toBeTruthy();
+  });
+
+  test('debe de decrementar con el botón -1', () => {
+    render(<CounterApp value={initialValue} />);
+    // fireEvent.click(screen.getByText('-1'));
+    fireEvent.click(screen.getByRole('button', {name: "btn-substract" }));
+    expect(screen.getByText('154')).toBeTruthy();
+  });
+
+  test('debe de funcionar el botón de reset', () => {
+    render(<CounterApp value={initialValue} />);
+    fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByText('+1'));
+    expect(screen.getByText('159')).toBeTruthy();
+    // fireEvent.click(screen.getByText('Reset'));
+    fireEvent.click(screen.getByRole('button', {name: "btn-reset" }));
+    expect(screen.getByText('155')).toBeTruthy();
+  });
 });
