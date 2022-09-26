5f8521b9d972df68ab660272c6ddf322f683b589
BorjaG90 <borjag90dev@gmail.com>
2022-09-26 20:01:03 +0200

    test(FirstApp.test*): update FirstApp tests #12

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -4,6 +4,17 @@ import { FirstApp } from '../src/FirstApp';
 describe('Pruebas en <FirstApp />', () => { 
   test('debe de hacer match con el snapshot', () => {
     const title = 'Hola, soy Goku';
-    render(<FirstApp title={title} />);
+    const {container} = render(<FirstApp title={title} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('debe de mostrar el título en un h2', () => {
+    const title = 'Hola, soy Goku';
+    const {container, getByText} = render(<FirstApp title={title} />);
+
+    expect(getByText(title)).toBeTruthy();
+    const h2 = container.querySelector('h2');
+    expect(h2.innerHTML).toContain(title);
   });
 });
