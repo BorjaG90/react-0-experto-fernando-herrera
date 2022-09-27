7d04d88a329ac29b98406ee27c5345489fc7fb50
BorjaG90 <borjag90dev@gmail.com>
2022-09-27 16:33:20 +0200

    feat(AddCategory.jsx): add AddCategory component #14

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
   const onAddCategory = () => {
-    setCategories([...categories, 'Valorant']);
+    setCategories(['Valorant', ...categories]);
   }
 
   return (
     <>
       <h1>GifExpertApp</h1>
+      <AddCategory />
       <button onClick={onAddCategory}>Agregar</button>
       <ol>
         {
---
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export const AddCategory = () => {
+  const [inputValue, setInputValue] = useState('One Punch');
+
+  const onInputChange = ({target}) => {
+    setInputValue(target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  )
+}
