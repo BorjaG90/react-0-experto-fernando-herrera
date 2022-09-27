4605ae8d05c1e3c46f537d9166fc0e3f0db4c244
BorjaG90 <borjag90dev@gmail.com>
2022-09-27 16:22:28 +0200

    feat(GifExpertApp.jsx): add new category #14

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,9 +3,14 @@ import { useState } from "react"
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
+  const onAddCategory = () => {
+    setCategories([...categories, 'Valorant']);
+  }
+
   return (
     <>
       <h1>GifExpertApp</h1>
+      <button onClick={onAddCategory}>Agregar</button>
       <ol>
         {
           categories.map((category) => {
