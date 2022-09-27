1c6dd6a7842174a31fbdb0e82d3a85ef270c10cd
BorjaG90 <borjag90dev@gmail.com>
2022-09-27 16:47:51 +0200

    feat(AddCategory): emit event to father #14

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -4,14 +4,17 @@ import { AddCategory } from "./components/AddCategory";
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
-  const onAddCategory = () => {
-    setCategories(['Valorant', ...categories]);
+  const onAddCategory = (newCategory) => {
+    setCategories([newCategory, ...categories]);
   }
 
   return (
     <>
       <h1>GifExpertApp</h1>
-      <AddCategory setCategories={setCategories} />
+      <AddCategory
+        // setCategories={setCategories}
+        onNewCategory={onAddCategory}
+      />
       <ol>
         {
           categories.map((category) => {
