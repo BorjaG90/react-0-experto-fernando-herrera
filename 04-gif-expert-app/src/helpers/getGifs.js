04017b54be82eadb719ee7ed2dab67c90c3e6597
BorjaG90 <borjag90dev@gmail.com>
2022-09-29 17:55:18 +0200

    feat(*): use a custom hook #14

--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,12 +1,16 @@
-export const getGifs = async(category) => {
-  const url = `api.giphy.com/v1/gifs/search?api_key=B20j9iunukSNMktQHeyg0mZHZHMP3BWl&q=${category}&limit=10`
+export const getGifs = async(category, api_key='B20j9iunukSNMktQHeyg0mZHZHMP3BWl', limit= 10) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=${limit}`
   const res = await fetch(url);
+  console.log(url);
+  console.log(res)
   const {data} = await res.json();
-  
+  console.log(data)
+
+
   const gifs = data.map( img => ({
     id: img.id,
     title: img.title,
-    url: img.downsized_medium.url
+    url: img.images.downsized_medium.url
   }));
   return gifs;
 }
