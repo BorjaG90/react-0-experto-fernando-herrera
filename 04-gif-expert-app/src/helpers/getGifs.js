937d99ea56e9669f617d8662650fd81e3235c9c4
BorjaG90 <borjag90dev@gmail.com>
2022-10-01 20:23:01 +0200

    revert(getGifs): remove logs #17

--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,11 +1,7 @@
 export const getGifs = async(category, api_key='B20j9iunukSNMktQHeyg0mZHZHMP3BWl', limit= 10) => {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=${limit}`
   const res = await fetch(url);
-  console.log(url);
-  console.log(res)
   const {data} = await res.json();
-  console.log(data)
-
 
   const gifs = data.map( img => ({
     id: img.id,
