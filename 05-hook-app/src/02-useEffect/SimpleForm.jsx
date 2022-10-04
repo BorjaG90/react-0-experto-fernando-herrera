3545b65287f326ef4b1376a1d7788aed6260b1b9
BorjaG90 <borjag90dev@gmail.com>
2022-10-04 12:40:52 +0200

    feat(*): useEffect - simple form #19

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { useState } from "react"
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: 'Borja',
+    email: 'borjag90dev@gmail.com'
+  });
+
+  const {username, email} = formState;
+
+  const onInputChange = ({target}) => {
+    const {name, value} = target;
+
+    setFormState({
+      ...formState,
+      [name]: value
+    })
+  };
+
+  useEffect(() => {
+    console.log('useEffect called!');
+  })
+
+
+  return (
+    <>
+      <h1>Formulario Simple</h1>
+      <hr />
+
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+    </>
+  )
+}
