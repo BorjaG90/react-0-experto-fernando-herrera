02d3bd9db1de0c932fdcbec5f34fad9b2f7dc3d8
BorjaG90 <borjag90dev@gmail.com>
2022-10-07 12:20:15 +0200

    feat(*): add form with custom hook #19

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -5,11 +5,12 @@ import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { HooksApp } from './HooksApp'
 import { SimpleForm } from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <SimpleForm />
+    <FormWithCustomHook />
   // </React.StrictMode>
 )
