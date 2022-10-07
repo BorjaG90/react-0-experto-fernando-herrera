4b587c00848af1d9f96ddedf7b6afaab33b10069
BorjaG90 <borjag90dev@gmail.com>
2022-10-07 13:07:22 +0200

    feat(MultipleCustomHooks) use useCounter hook #19

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,7 +1,10 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
-  const {data, isLoading, hasError} = useFetch('https://www.breakingbadapi.com/api/quotes/1');
+  const { counter, increment } = useCounter(1);
+  
+  const {data, isLoading, hasError} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
   
   const {author, quote} = !!data && data[0];
 
@@ -23,7 +26,10 @@ export const MultipleCustomHooks = () => {
         )
       }
 
-      <button className="btn btn-primary">Next quote</button>
+      <button
+        className="btn btn-primary"
+        onClick={() => increment()}
+      >Next quote</button>
     </>
   )
 }
