dafee4b806010b4f7debc83846f17e481d10b0bc
BorjaG90 <borjag90dev@gmail.com>
2022-10-08 18:35:24 +0200

    feat(*): useCallback #19

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -10,7 +10,8 @@ import ReactDOM from 'react-dom/client'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-memos/Memorize'
-import { MemoHook } from './06-memos/MemoHook'
+// import { MemoHook } from './06-memos/MemoHook'
+import { CallbackHook } from './06-memos/CallbackHook'
 
 
 
@@ -18,6 +19,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MemoHook/>
+    <CallbackHook/>
   // </React.StrictMode>
 )
