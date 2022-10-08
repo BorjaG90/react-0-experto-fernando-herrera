4c21bb925de48969af62c0777756903259307c81
BorjaG90 <borjag90dev@gmail.com>
2022-10-08 18:46:41 +0200

    feat(*): add Memorize task #19

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -12,13 +12,12 @@ import ReactDOM from 'react-dom/client'
 // import { Memorize } from './06-memos/Memorize'
 // import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
-
-
+import { Padre } from './07-tarea-memo/Padre'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallbackHook/>
+    <Padre/>
   // </React.StrictMode>
 )
