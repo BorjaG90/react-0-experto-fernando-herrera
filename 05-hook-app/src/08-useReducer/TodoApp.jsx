9edc1d300065ff88f2d5089b9e48079d6b4bff38
BorjaG90 <borjag90dev@gmail.com>
2022-10-16 13:54:01 +0200

    feat(*): add useTodo task resolution #21

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,49 +1,10 @@
-import { useEffect, useReducer } from "react"
-import { TodoAdd } from "./TodoAdd";
-import { TodoList } from "./TodoList";
-import { todoReducer } from "./todoReducer";
+import { useTodos } from "../hooks/useTodos";
+import { TodoAdd , TodoList } from "./";
 
-const initialState = [
-  // {
-  //   id: new Date().getTime(),
-  //   description: 'Recolectar la piedra del Alma',
-  //   done: false
-  // },
-];
 
-const init = () => {
-  return JSON.parse(localStorage.getItem('todos') || []);
-}
 
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos])
-
-  const handleNewTodo = (newTodo) => {
-    const action = {
-      type: '[TODO] Add ToDo',
-      payload: newTodo
-    };
-
-    dispatch(action);
-  }
-
-  const handleDeleteTodo = (id) => {
-    dispatch({
-      type: '[TODO] Remove ToDo',
-      payload: id
-    });
-  }
-
-  const handleToggleTodo = (id) => {
-    dispatch({
-      type: '[TODO] Toggle ToDo',
-      payload: id
-    });
-  }
+  const {todos, handleNewTodo, handleDeleteTodo, handleToggleTodo} = useTodos();  
 
   return (
     <>
