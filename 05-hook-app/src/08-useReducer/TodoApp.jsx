ba4c8f7bb0379f3854da7aa1b8c61a1545dff93e
BorjaG90 <borjag90dev@gmail.com>
2022-10-16 13:34:43 +0200

    feat(*): delete a ToDo #21

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -21,7 +21,6 @@ export const TodoApp = () => {
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos])
-  
 
   const handleNewTodo = (newTodo) => {
     const action = {
@@ -32,6 +31,13 @@ export const TodoApp = () => {
     dispatch(action);
   }
 
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: '[TODO] Remove ToDo',
+      payload: id
+    });
+  }
+
   return (
     <>
       <h1>TodoApp (10), <small>pendientes: 2</small></h1>
@@ -39,7 +45,7 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
         </div>
 
         <div className="col-5">
--- 05-hook-app/src/08-useReducer/todoReducer.js
+++ 05-hook-app/src/08-useReducer/todoReducer.js
@@ -1,9 +1,12 @@
 export const todoReducer = (initialState, action) => {
   switch (action.type) {
     case '[TODO] Add ToDo':
-      return [...initialState, action.payload]
-      
-      default:
+      return [...initialState, action.payload];
+
+    case '[TODO] Remove ToDo':
+      return initialState.filter(todo => todo.id !== action.payload);
+
+    default:
       return initialState;
   }
 }
