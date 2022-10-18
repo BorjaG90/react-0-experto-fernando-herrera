7039b3788fcd817c215758e8abeb9e21b668bb78
BorjaG90 <borjag90dev@gmail.com>
2022-10-18 21:02:05 +0200

    feat(Navbar.jsx): add NavLink #23

--- 05-hook-app/src/09-useContext/Navbar.jsx
+++ 05-hook-app/src/09-useContext/Navbar.jsx
@@ -1,11 +1,26 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export const Navbar = () => {
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
-    </>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to={"/"}>UseContext</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <NavLink 
+              className="nav-link"
+              to={"/about"}
+            >About</NavLink>
+            <NavLink 
+              className="nav-link"
+              to={"/login"}
+            >Login</NavLink>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
