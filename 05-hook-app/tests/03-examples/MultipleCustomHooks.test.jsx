f6394e2855e9c302477f8647b57fbd785fa87274
BorjaG90 <borjag90dev@gmail.com>
2022-10-21 12:43:48 +0200

    test(MultipleCustomHooks.test): add useFetch evaluation tests #25

--- 05-hook-app/tests/03-examples/MultipleCustomHooks.test.jsx
+++ 05-hook-app/tests/03-examples/MultipleCustomHooks.test.jsx
@@ -1,8 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MultipleCustomHooks } from "../../src/03-examples/MultipleCustomHooks";
+import { useFetch } from "../../src/hooks/useFetch";
+import { useCounter } from "../../src/hooks/useCounter";
+
+jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter')
 
 describe('Pruebas en <MultipleCustomHooks/>', () => {
+  const mockIncrement = jest.fn();
+  useCounter.mockReturnValue({
+    counter: 1,
+    increment: mockIncrement
+  });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  })
+
   test('should de mostrar el componente por defecto', () => {
+    useFetch.mockReturnValue({data: null, isLoading: true, hasError: null});
     render(<MultipleCustomHooks />);
 
     expect(screen.getByText('Loading...'));
@@ -11,5 +26,36 @@ describe('Pruebas en <MultipleCustomHooks/>', () => {
     const nextButton = screen.getByRole('button', {name: 'Next quote'})
     
     expect(nextButton.disabled).toBeTruthy();
+  });
+
+  test('should de mostrar un quote', () => {
+    useFetch.mockReturnValue({
+      data: [{author: 'Borja', quote: 'Hola mundo'}],
+      isLoading: false,
+      hasError: null
+    });
+    render(<MultipleCustomHooks />);
+
+    expect(screen.getByText('Hola mundo')).toBeTruthy();
+    expect(screen.getByText('Borja')).toBeTruthy();
+
+    const nextButton = screen.getByRole('button', {name: 'Next quote'})
+    
+    expect(nextButton.disabled).toBeFalsy();
+  });
+
+  test('should de llamar la función de incrementar', () => {
+    useFetch.mockReturnValue({
+      data: [{author: 'Borja', quote: 'Hola mundo'}],
+      isLoading: false,
+      hasError: null
+    });
+    
+    render(<MultipleCustomHooks />);
+    const nextButton = screen.getByRole('button', {name: 'Next quote'})
+    fireEvent.click(nextButton);
+
+    expect(mockIncrement).toHaveBeenCalled()
+
   });
 });
