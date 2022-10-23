2c67620c809092d5457b49adb40f99b0b06c3c87
BorjaG90 <borjag90dev@gmail.com>
2022-10-23 10:50:59 +0200

    test(LoginPage*): add LoginPage tests #25

--- 05-hook-app/src/09-useContext/LoginPage.jsx
+++ 05-hook-app/src/09-useContext/LoginPage.jsx
@@ -9,7 +9,7 @@ export const LoginPage = () => {
       <h2>LoginPage</h2>
       <hr />
 
-      <pre>
+      <pre aria-label="pre">
         {JSON.stringify(user, null, 3)}
       </pre>
 
---
+++ 05-hook-app/tests/09-useContext/LoginPage.test.jsx
@@ -0,0 +1,33 @@
+import {fireEvent, render, screen} from '@testing-library/react';
+import { LoginPage } from '../../src/09-useContext';
+import { UserContext } from '../../src/09-useContext/context/UserContext';
+describe('Pruebas en <LoginPage>', () => {
+  test('should de mostrar el componente sin el usuario', () => {
+    render(
+      <UserContext.Provider value={{user: null}}>
+        <LoginPage />
+      </UserContext.Provider>
+    );
+
+    const preTag = screen.getByLabelText('pre'); //aria-label
+
+    expect(preTag.innerHTML).toBe('null');
+  });
+
+  test('should de llamar al setUser cuando se hace el click', () => {
+    const setUserMock = jest.fn();
+
+    render(
+      <UserContext.Provider value={{user: null, setUser: setUserMock}}>
+        <LoginPage />
+      </UserContext.Provider>
+    );
+
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    expect(setUserMock).toHaveBeenCalledWith(
+      {"email": "juan@foo.com", "id": 1, "name": "Juan"}
+    );
+  });
+});
