a353b323159475b755e78d0543eab201d20f152c
BorjaG90 <borjag90dev@gmail.com>
2022-11-06 12:07:49 +0100

    feat(*): add useMemo #27

--- 07-heroes-spa/src/heroes/components/HeroList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,9 +1,9 @@
+import { useMemo } from "react";
 import { getheroesByPublisher } from "../helpers"
 import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({publisher}) => {
-  const heroes = getheroesByPublisher(publisher);
-
+  const heroes = useMemo(() => getheroesByPublisher(publisher), [publisher]);
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
       {heroes.map(hero => (
