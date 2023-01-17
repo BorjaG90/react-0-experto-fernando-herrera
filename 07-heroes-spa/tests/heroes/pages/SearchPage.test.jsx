b756366f21e0d729986da18c5148cdcb41d83db5
BorjaG90 <borjag90dev@gmail.com>
2023-01-17 13:12:43 +0100

    test(SearchPage*): add SearchPage error & navigate test #31

--- 07-heroes-spa/tests/heroes/pages/SearchPage.test.jsx
+++ 07-heroes-spa/tests/heroes/pages/SearchPage.test.jsx
@@ -1,9 +1,17 @@
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { SearchPage } from '../../../src/heroes/pages/SearchPage';
 
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUseNavigate // Solo se sobrescribe el useNavigate
+}));
 
 describe('Pruebas en <SearchPage />', () => {
+  beforeEach(() => jest.clearAllMocks());
+
   test('debe de mostrarse correctamente con valores por defecto', () => {
     const {container} = render(
       <MemoryRouter>
@@ -31,6 +39,35 @@ describe('Pruebas en <SearchPage />', () => {
 
     const showError = screen.getByLabelText('show-error');
     expect(showError.style.display).toBe('none');
+  });
 
+  test('debe de mostrar un error si no se encuentra el heroe', () => {
+    const heroValue = 'fakeHeroe123';
+
+    render(
+      <MemoryRouter initialEntries={[`/search?q=${heroValue}`]}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+
+    const showError = screen.getByLabelText('show-error');
+    expect(showError.style.display).not.toBe('none');
+  });
+
+  test('debe de llamar el navigate a la pantalla nueva de busqueda', () => {
+    const inputValue = 'superman';
+    render(
+      <MemoryRouter initialEntries={['/search']}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByRole('textbox');
+    fireEvent.change(input, {target: {name: 'searchText', value: inputValue}});
+
+    const form = screen.getByRole('form');
+    fireEvent.submit(form);
+
+    expect(mockedUseNavigate).toHaveBeenCalledWith(`?q=${inputValue}`);
   });
 });
