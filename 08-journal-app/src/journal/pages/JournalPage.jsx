e92b84327a7098b42806d9ef9779271adcd579f6
BorjaG90 <borjag90dev@gmail.com>
2023-01-23 09:18:29 +0100

    feat(*): add NothingSelectedView #33

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,14 +1,10 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <Typography variant='h1'>JournalPage</Typography> */}
-      <Typography>Laboris magna excepteur duis amet ea ullamco et exercitation deserunt nulla ex qui. Et irure nostrud sint minim duis. Lorem irure mollit ipsum aliquip dolore quis nulla eu enim duis. Occaecat sunt qui nostrud laboris.
-
-Sunt laborum sunt nulla consequat sint duis occaecat sint voluptate. Mollit Lorem nulla Lorem esse commodo do. Officia ad culpa voluptate duis. Fugiat laboris incididunt ea officia aliquip incididunt sunt.</Typography>
-
+      <NothingSelectedView />
     </JournalLayout>
   )
 }
