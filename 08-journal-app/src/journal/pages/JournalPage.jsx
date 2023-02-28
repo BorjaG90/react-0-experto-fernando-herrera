2ed7d7e6f1479695014e1b5e3d151dfe06055033
BorjaG90 <borjag90dev@gmail.com>
2023-02-28 13:50:14 +0100

    feat(*): add create new note #39

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -2,14 +2,21 @@ import { IconButton } from '@mui/material';
 import { AddOutlined } from '@mui/icons-material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { useDispatch } from 'react-redux';
+import { startNewNote } from '../../store/journal';
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  }
   return (
     <JournalLayout>
       <NothingSelectedView />
       {/* <NoteView /> */}
 
       <IconButton
+      onClick={onClickNewNote}
         size='large'
         sx={{
           color: 'white',
