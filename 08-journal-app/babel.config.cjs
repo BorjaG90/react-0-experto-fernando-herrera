fbd2b75f7e80af35827951112c1b6f032a1e2e66
BorjaG90 <borjag90dev@gmail.com>
2023-03-15 11:42:00 +0100

    chore(*): add test config #41

---
+++ 08-journal-app/babel.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+          esmodules: true
+        },
+      },
+    ],
+    [
+      "@babel/preset-react",
+      {
+        runtime: 'automatic'
+      }
+    ]
+  ],
+};
