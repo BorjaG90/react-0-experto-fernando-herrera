ac8f1b18863a01513937327690c57e1cd56c2182
BorjaG90 <borjag90dev@gmail.com>
2023-03-30 19:03:04 +0200

    chore(config.js): add testing config #41

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -6,13 +6,26 @@ import { getFirestore } from 'firebase/firestore/lite'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Dev/Prod
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDed5DS_t9kTaezHOgpBpeZJHyyjjbEjB8",
+//   authDomain: "react-fer-her.firebaseapp.com",
+//   projectId: "react-fer-her",
+//   storageBucket: "react-fer-her.appspot.com",
+//   messagingSenderId: "668090560685",
+//   appId: "1:668090560685:web:e6869c5b4968045ced9624"
+// };
+
+// Test
 const firebaseConfig = {
-  apiKey: "AIzaSyDed5DS_t9kTaezHOgpBpeZJHyyjjbEjB8",
-  authDomain: "react-fer-her.firebaseapp.com",
-  projectId: "react-fer-her",
-  storageBucket: "react-fer-her.appspot.com",
-  messagingSenderId: "668090560685",
-  appId: "1:668090560685:web:e6869c5b4968045ced9624"
+  apiKey: "AIzaSyChyLPSBDu7wzQccWvE-tZoezVv2WPe6PM",
+  authDomain: "react-my-burger-a01b3.firebaseapp.com",
+  databaseURL: "https://react-my-burger-a01b3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "react-my-burger-a01b3",
+  storageBucket: "react-my-burger-a01b3.appspot.com",
+  messagingSenderId: "729923226809",
+  appId: "1:729923226809:web:12bf8b1caea313536adf68",
+  measurementId: "G-159R130ZSV"
 };
 
 // Initialize Firebase
