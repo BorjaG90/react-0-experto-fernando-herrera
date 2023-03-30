09f5decbc312391f8b62483d02e3a9693f1ad1a7
BorjaG90 <borjag90dev@gmail.com>
2023-03-30 19:31:47 +0200

    chore(*): add test & dev env #41

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -2,9 +2,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore/lite'
+import { getEnviroments } from "../helpers";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const env = getEnviroments();
 // Your web app's Firebase configuration
 // Dev/Prod
 // const firebaseConfig = {
