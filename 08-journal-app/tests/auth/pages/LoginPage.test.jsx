09c706a950721d4a2d15195c34694eef9ea8216b
BorjaG90 <borjag90dev@gmail.com>
2023-03-31 13:12:12 +0200

    test(*): add GoogleSIgnIn tests #41

--- 08-journal-app/tests/auth/pages/LoginPage.test.jsx
+++ 08-journal-app/tests/auth/pages/LoginPage.test.jsx
@@ -1,16 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 
 import { LoginPage } from '../../../src/auth/pages/LoginPage';
-import { authSlice } from '../../../src/store/auth';
+import { authSlice } from '../../../src/store/auth/';
+import { startGoogleSignIn } from '../../../src/store/auth/thunks';
+import { notAuthenticatedState } from '../../fixtures/authFixtures';
+
+const mockStartGoogleSignIn = jest.fn();
+jest.mock('../../../src/store/auth/thunks', () => {
+  startGoogleSignIn: () => mockStartGoogleSignIn
+});
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer
   },
-  // preloadedState
+  preloadedState: {
+    auth: notAuthenticatedState
+  }
 });
 
 describe('Pruebas en el <LoginPage/>', () => {
@@ -23,7 +32,20 @@ describe('Pruebas en el <LoginPage/>', () => {
       </Provider>
     );
 
-    // screen.debug();
     expect(screen.getAllByText('Login').length).toBeGreaterThanOrEqual(1);
+  });
+
+  test('Google debe de llamar a startGoogleSignIn', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <LoginPage/>
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const googleBtn = screen.getByLabelText('google-btn');
+    fireEvent.click(googleBtn);
+    expect(mockStartGoogleSignIn).toHaveBeenCalled();
   });
 });
