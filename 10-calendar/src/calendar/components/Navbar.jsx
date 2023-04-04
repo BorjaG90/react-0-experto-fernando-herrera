0b4f0774e31e5ccf7ae023b70fdeb237929aeabb
BorjaG90 <borjag90dev@gmail.com>
2023-04-04 13:51:36 +0200

    feat(*): add LoginPage & Navbar  #43

---
+++ 10-calendar/src/calendar/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4 px-4">
+      <span className="">
+        <i className="fas fa-calendar-alt"></i>
+        &nbsp;
+        Borja
+      </span>
+      <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt"></i>
+        <span>Salir</span>
+      </button>
+    </div>
+  )
+}
