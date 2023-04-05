89a75e7db4c1a498b5309eca6635649d560c73fd
BorjaG90 <borjag90dev@gmail.com>
2023-04-05 20:50:16 +0200

    feat(*): add CalendarModal #43

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -4,7 +4,7 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours } from 'date-fns';
 
-import { CalendarEvent, Navbar } from '../';
+import { CalendarEvent, CalendarModal, Navbar } from '../';
 import { localizer, getMessagesES } from '../../helpers';
 useState
 
@@ -69,6 +69,7 @@ export const CalendarPage = () => {
         onSelectEvent={ onSelect }
         onView={ onViewChanged }
       />
+      <CalendarModal />
     </>
   )
 }
