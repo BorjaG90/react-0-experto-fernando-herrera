746e7b772ff3f2fbf688e62825ebe3104876e966
BorjaG90 <borjag90dev@gmail.com>
2023-04-08 12:11:50 +0200

    feat(CalendarModal): get event form info #43

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { addHours } from 'date-fns';
+import { addHours, differenceInSeconds } from 'date-fns';
 
 import Modal from 'react-modal';
 import DatePicker, { registerLocale } from 'react-datepicker';
@@ -46,6 +46,24 @@ export const CalendarModal = () => {
     console.log('cerrando modal');
     setIsModalOpen(false);
   };
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const difference = differenceInSeconds(formValues.end, formValues.start);
+    
+    if (isNaN(difference) || difference <= 0) {
+      console.log('Error en fechas');
+      return;
+    }
+
+    if (formValues.title.length <= 0) return;
+
+    console.log(formValues);
+
+    // TODO:
+    // cerrar modal
+    // Remover errores en pantalla
+  }
 
   return (
     <Modal
@@ -58,7 +76,7 @@ export const CalendarModal = () => {
     >
       <h1> Nuevo evento </h1>
       <hr />
-      <form className="container">
+      <form className="container" onSubmit={onSubmit}>
           <div className="form-group mb-2">
               <label>Fecha y hora inicio</label>
               <DatePicker
