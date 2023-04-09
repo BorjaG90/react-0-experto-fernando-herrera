1d7983ddd19087c2e73329abddf337688e1aaaab
BorjaG90 <borjag90dev@gmail.com>
2023-04-09 10:32:45 +0200

    feat(*): add update event #43

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -80,7 +80,6 @@ export const CalendarModal = () => {
 
     console.log(formValues);
 
-    // TODO:
     await startSavingEvent(formValues);
     closeDateModal();
     setFormSubmitted(false);
--- 10-calendar/src/hooks/useCalendarStore.js
+++ 10-calendar/src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { onSetActiveEvent, onAddNewEvent } from '../store/calendar/calendarSlice';
+import { onSetActiveEvent, onAddNewEvent, onUpdateEvent } from '../store/calendar/calendarSlice';
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
@@ -12,9 +12,9 @@ export const useCalendarStore = () => {
   const startSavingEvent = async(calendarEvent) => {
     // TODO: llegar al backend
 
-    // Correcto
     if (calendarEvent._id) {
       // Actualizando
+      dispatch(onUpdateEvent(calendarEvent));
     } else {
       // Creando
       dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime}));
