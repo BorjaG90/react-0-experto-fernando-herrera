7983704256dc6ecd0b312b485d7508037ed1033d
BorjaG90 <borjag90dev@gmail.com>
2023-04-10 16:33:42 +0200

    feat(*): add auth endpoints #45

--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -6,10 +6,10 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    ok: true
-  });
-});
+const {createUser, loginUser, renewToken} = require('../controllers/auth');
+
+router.post('/', loginUser);
+router.post('/new', createUser);
+router.get('/renew', renewToken);
 
 module.exports = router;
