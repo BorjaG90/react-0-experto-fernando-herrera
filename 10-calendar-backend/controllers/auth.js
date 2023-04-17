c8e73343d7ad032f9cd06151b39cf5b5910e65ee
BorjaG90 <borjag90dev@gmail.com>
2023-04-17 16:52:16 +0200

    feat(*): add express validator #45

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,12 +1,16 @@
 const {response} = require('express');
+const {validationResult} = require('express-validator');
 
 const createUser = (req, res = response) => {
   const {name, email, password} = req.body;
 
-  if (name.length < 5) {
+  // manejo de errores
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
     return res.status(400).json({
       ok: false,
-      msg: 'El nombre debe ser mayor de 5 letras'
+      errors: errors.mapped()
     })
   }
 
@@ -22,7 +26,17 @@ const createUser = (req, res = response) => {
 const loginUser = (req, res = response) => {
   const {email, password} = req.body;
 
-  res.json({
+  // manejo de errores
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    })
+  }
+
+  res.status(201).json({
     ok: true,
     msg: 'login',
     email,
--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -4,12 +4,32 @@
 */
 
 const {Router} = require('express');
+const {check} = require('express-validator');
 const router = Router();
 
 const {createUser, loginUser, renewToken} = require('../controllers/auth');
 
-router.post('/', loginUser);
-router.post('/new', createUser);
+router.post(
+  '/',
+  [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres')
+    .isLength({min: 6}),
+  ],
+  loginUser
+);
+
+router.post(
+  '/new',
+  [ // middlewares
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres')
+      .isLength({min: 6}),
+  ],
+  createUser
+);
+
 router.get('/renew', renewToken);
 
 module.exports = router;
