21da59558b3ffc93dc9a5edb3717c01434a9d5dd
BorjaG90 <borjag90dev@gmail.com>
2023-06-20 11:39:42 +0200

    feat(*): add custom validations #47

--- 10-calendar-backend/routes/events.js
+++ 10-calendar-backend/routes/events.js
@@ -4,8 +4,11 @@
 */
 
 const {Router} = require('express');
+const {check} = require('express-validator');
+const {fieldValidate} = require('../middlewares/field-validator');
 const {jwtValidate} = require('../middlewares/jwt-validator');
-const {getEvents, createEvent, updateEvent, deleteEvent} = require('../controllers/events')
+const {getEvents, createEvent, updateEvent, deleteEvent} = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -16,7 +19,16 @@ router.use(jwtValidate);
 router.get('/', getEvents);
 
 // Crear un nuevo evento
-router.post('/', createEvent);
+router.post(
+  '/',
+  [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de fin es obligatoria').custom(isDate),
+    fieldValidate
+  ],
+  createEvent
+);
 
 // Actualizar evento
 router.put('/:id', updateEvent);
