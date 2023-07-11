8fda9f817738c9c49466eb1c6fcd319efc2fdf2f
BorjaG90 <borjag90dev@gmail.com>
2023-07-11 12:06:36 +0200

    feat(LoginPage): add show authentication error #49

--- 10-calendar/src/auth/pages/LoginPage.jsx
+++ 10-calendar/src/auth/pages/LoginPage.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from 'react';
+import Swal from 'sweetalert2';
 import { useForm, useAuthStore } from '../../hooks';
+
 import './LoginPage.css';
 
 const loginFormFields = {
@@ -14,7 +17,7 @@ const registerFormFields = {
 }
 
 export const LoginPage = () => {
-  const {startLogin} = useAuthStore();
+  const {startLogin, errorMessage} = useAuthStore();
   const {loginEmail, loginPassword, onInputChange: onLoginInputChange} = useForm(loginFormFields);
   const {registerName, registerEmail, registerPassword, registerPassword2, onInputChange: onRegisterInputChange} = useForm(registerFormFields);
 
@@ -27,6 +30,13 @@ export const LoginPage = () => {
     event.preventDefault();
     console.log({registerName, registerEmail, registerPassword, registerPassword2});
   }
+
+  useEffect(() => {
+    if (errorMessage !== undefined) {
+      Swal.fire('Error en la autenticación', errorMessage, 'error');
+    }
+  }, [errorMessage])
+  
 
   return (
     <div className="container login-container">
