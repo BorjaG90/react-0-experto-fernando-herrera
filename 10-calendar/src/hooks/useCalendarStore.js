28dc81932486405320cf5019c997ec2a08d21f58
BorjaG90 <borjag90dev@gmail.com>
2023-07-11 13:30:27 +0200

    feat(*): add load events to store #51

--- 10-calendar/src/hooks/useCalendarStore.js
+++ 10-calendar/src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } from '../store/calendar/calendarSlice';
+import { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent, onLoadEvents } from '../store/calendar/calendarSlice';
 import { calendarApi } from '../api';
 import { convertEventstoDateEvents } from '../helpers';
 
@@ -38,6 +38,7 @@ export const useCalendarStore = () => {
     try {
       const {data} = await calendarApi.get('/events');
       const events = convertEventstoDateEvents(data.events);
+      dispatch(onLoadEvents(events));
       console.log(events);
     } catch (error) {
       console.log('Error cargando eventos');
