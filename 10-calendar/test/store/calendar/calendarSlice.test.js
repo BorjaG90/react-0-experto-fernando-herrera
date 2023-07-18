ad032b47019260a1400634950eb57a336a6425f3
BorjaG90 <borjag90dev@gmail.com>
2023-07-18 13:08:58 +0200

    test(calendarSlice.test): add calendar slice test #53

---
+++ 10-calendar/test/store/calendar/calendarSlice.test.js
@@ -0,0 +1,39 @@
+import { calendarSlice, onAddNewEvent, onSetActiveEvent, onUpdateEvent } from "../../../src/store/calendar/calendarSlice";
+import { calendarWithActiveEventState, calendarWithEventsState, events, initialState } from "../../fixtures/calendarStates";
+
+describe('Pruebas en calendarSlice', () => {
+  test('debe de regresar el estado por defecto', () => {
+    expect(calendarSlice.getInitialState()).toEqual(initialState);
+  });
+
+  test('onSetActiveEvent de activar el event', () => {
+    const state = calendarSlice.reducer(calendarWithEventsState, onSetActiveEvent(events[0]));
+    expect(state.activeEvent).toEqual(events[0]);
+  });
+
+  test('onAddNewEvent debe de agregar el evento', () => {
+    const newEvent = {
+      id: '3',
+      title: 'Cumpleaños de Fer',
+      notes: 'Alguna nota de Fer',
+      start: new Date('2022-04-05 13:00:00'),
+      end: new Date('2022-04-05 15:00:00')
+    };
+
+    const state = calendarSlice.reducer(calendarWithEventsState, onAddNewEvent(newEvent));
+    expect(state.events).toEqual([...events, newEvent]);
+  });
+
+  test('onUpdateEvent debe de actualizar el evento', () => {
+    const updatedEvent = {
+      id: '1',
+      title: 'Cumpleaños de Alguien',
+      notes: 'Alguna nota de Alguien',
+      start: new Date('2022-04-05 13:00:00'),
+      end: new Date('2022-04-05 15:00:00')
+    };
+
+    const state = calendarSlice.reducer(calendarWithEventsState, onUpdateEvent(updatedEvent));
+    expect(state.events).toContain(updatedEvent);
+  });
+});
